f2595e087d2334f3fec83d783db0ca9384aace97
rafael sena <89745814+rxfxelx@users.noreply.github.com>
2025-09-12 15:42:25 -0300

    Create routes.go

---
+++ routes.go
@@ -0,0 +1,5 @@
+package main
+
+// Este arquivo agrega documentação e serve como ponto de organização de rotas.
+// As rotas da integração WhatsApp estão em wa_instances_api.go.
+// Demais módulos (auth, catálogo, leads, etc.) permanecem onde já foram implementados.
